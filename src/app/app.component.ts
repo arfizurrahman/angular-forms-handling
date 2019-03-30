17b8ce4c16111833297c8f1df4834df51f7c75cd
Arfizur Rahman <arfizrahman0@gmail.com>
2019-03-30 15:04:39 +0600

    Reset form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,5 +47,7 @@ submitted = false;
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
